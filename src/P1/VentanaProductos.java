beb42a33c332bff7444afdd9a4c912db27ccda24
collgonzalo06@gmail.com <collgonzalo06@gmail.com>
2025-09-10 19:24:44 -0500

    Cambie la library

--- src/P1/VentanaProductos.java
+++ src/P1/VentanaProductos.java
@@ -47,7 +47,7 @@ public class VentanaProductos extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public VentanaProductos() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 482, 592);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
